59f2b377dec36a2ef4a94311f61669dc37a75e9e
Skarnisk <aaronperezclase@gmail.com>
2019-06-27 10:13:06 +0100

    rechufleiba

---
+++ empleado.py
@@ -0,0 +1,22 @@
+class Empleado:
+    def_init_(self, nombre, salario):
+        self._nombre = nombre
+        self._salario = salario
+
+    def getnombre(self):
+        return self._nombre
+
+    def getsalario(self):
+        return self._salario
+
+    def setnombre(self, nombre):
+        self._nombre = nombre
+
+    def setsalario(self, salario):
+        self._salario = salario
+
+    def delnombre(self):
+        del self._nombre
+
+    def delsalario(self):
+        del self._salario
--- principal.py
+++ principal.py
@@ -12,3 +12,14 @@ dedo().tamanio = "dedo grande"
 x = NuevoObjeto()
 print (x)
 print (x.)
+
+compra1 = Factura(12, 110)
+print(compra1.unidad)
+print(compra1.precio)
+print(compra1.a_pagar(), "euros")
+print(Factura._tasa) # error
+
+empleado1 = Empleado("Francisco",30000)
+print(empleado1.getnombre())
+empleado1.setnombre("Francisco José")
+print(empleado1.getnombre(),",", empleado1.getsalario())
